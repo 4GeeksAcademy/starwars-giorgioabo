b290b99d1f0324a73fc92405d095a1b11a6b849f
Giorgio <155197460+giorgioabo@users.noreply.github.com>
2024-10-11 23:30:35 +0000

    done

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
-
 import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import { VehiclesDetails } from "./views/vehiclesDetails";
+import { CharactersDetails } from "./views/charactersDetails";
+import { PlanetsDetails } from "./views/planetsDetails";
 
-//create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	
 	const basename = process.env.BASENAME || "";
 
 	return (
@@ -23,11 +19,16 @@ const Layout = () => {
 					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
+						<Route path="/vehicles/:id" element={<VehiclesDetails />} />
+						<Route path="/characters/:id" element={<CharactersDetails />} />
+						<Route path="/planets/:id" element={<PlanetsDetails />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
-					<Footer />
+					<footer className="footer mt-auto py-3 text-center">
+						<span>
+							Made by <strong>Giorgio Behna</strong>. Copyright ©2024.
+						</span>
+					</footer>	
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,101 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+    return {
+        store: {
+            characters: [],
+            validCharacters: [],
+            characterDetails: [],
+            vehicles: [],
+            validVehicles: [],
+            vehiclesDetails: [],
+            planets: [],
+            validPlanets: [],
+            planetsDetails: [],
+            favorites: [],
+            invalidImages: []
+        },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        actions: {
+            getCharacters: async () => {
+                const res = await fetch(`https://www.swapi.tech/api/people/`);
+                const data = await res.json();
+                const validChars = await Promise.all(
+                    data.results.map(async (item) => {
+                        const isValid = await getActions().checkImageURL(`https://starwars-visualguide.com/assets/img/characters/${item.uid}.jpg`);
+                        return isValid ? item : null;
+                    })
+                );
+                setStore({ characters: data.results, validCharacters: validChars.filter(Boolean) });
+            },
+            getCharactersDetails: async (id) => {
+                const result = await fetch(`https://www.swapi.tech/api/people/${id}`);
+                const data = await result.json();
+                setStore({ characterDetails: data.result.properties });
+            },
+            getVehicles: async () => {
+                const res = await fetch(`https://www.swapi.tech/api/vehicles/`);
+                const data = await res.json();
+                const validVehs = await Promise.all(
+                    data.results.map(async (item) => {
+                        const isValid = await getActions().checkImageURL(`https://starwars-visualguide.com/assets/img/vehicles/${item.uid}.jpg`);
+                        return isValid ? item : null;
+                    })
+                );
+                setStore({ vehicles: data.results, validVehicles: validVehs.filter(Boolean) });
+            },
+            getVehiclesDetails: async (id) => {
+                const result = await fetch(`https://www.swapi.tech/api/vehicles/${id}`);
+                const data = await result.json();
+                setStore({ vehiclesDetails: data.result.properties });
+            },
+            getPlanets: async () => {
+                const res = await fetch(`https://www.swapi.tech/api/planets/`);
+                const data = await res.json();
+                const validPlts = await Promise.all(
+                    data.results.map(async (item) => {
+                        if (item.uid === "1" && getStore().invalidImages.includes("1")) return null;
+                        const isValid = await getActions().checkImageURL(`https://starwars-visualguide.com/assets/img/planets/${item.uid}.jpg`);
+                        return isValid ? item : null;
+                    })
+                );
+                setStore({ planets: data.results, validPlanets: validPlts.filter(Boolean) });
+            },
+            getPlanetsDetails: async (id) => {
+                const result = await fetch(`https://www.swapi.tech/api/planets/${id}`);
+                const data = await result.json();
+                setStore({ planetsDetails: data.result.properties });
+            },
+            checkImageURL: async (url) => {
+                try {
+                    const response = await fetch(url);
+                    if (response.status === 404 && url.includes("1.jpg")) {
+                        const invalidImages = getStore().invalidImages;
+                        setStore({ invalidImages: [...invalidImages, "1"] });
+                    }
+                    return response.ok;
+                } catch (error) {
+                    console.error(`Error fetching image from ${url}:`, error);
+                    return false;
+                }
+            },
+            toggleFavorite: (item) => {
+                const store = getStore();
+                const favorites = store.favorites;
+                const index = favorites.findIndex(fav => fav.uid === item.uid);
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+                if (index === -1) {
+                    setStore({ favorites: [...favorites, item] });
+                } else {
+                    const updatedFavorites = favorites.filter(fav => fav.uid !== item.uid);
+                    setStore({ favorites: updatedFavorites });
+                }
+            },
+            removeFavorite: (uid) => {
+                const store = getStore();
+                const updatedFavorites = store.favorites.filter(favorite => favorite.uid !== uid);
+                setStore({ favorites: updatedFavorites });
+            }
+        }
+    };
 };
 
 export default getState;
